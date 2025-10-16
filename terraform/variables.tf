6f23ff26d2ae2bd0ab1ac96be516c0b00c2f2cef
Shiv Gupta <shivgupta3635@gmail.com>
2025-10-16 09:29:03 +0530

    Fix variables.tf after resolving merge conflicts

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
-
-variable "instance_count" {
-  type    = number
-  default = 3
-}
-
-variable "instance_type" {
-  type    = string
-  default = "t3.micro"
-}
-
-variable "ami_id" {
-  type    = string
-  description = "Ubuntu 22.04 AMI id for the chosen region"
-}
-
-variable "key_name" {
-  type = string
-}
-
-variable "public_key_path" {
-  type = string
-}
-
-variable "ssh_user" {
-  type = string
-  default = "ubuntu"
-}
-
-variable "my_ip_cidr" {
-  type = string
-  default = "0.0.0.0/0"
-=======
 # AWS region where instances will be created
 variable "aws_region" {
   type        = string
@@ -87,5 +49,4 @@ variable "my_ip_cidr" {
   type        = string
   default     = "0.0.0.0/0"
   description = "IP range allowed to SSH into the instances (default allows all)"
->>>>>>> 5a9f615 (variable)
 }
